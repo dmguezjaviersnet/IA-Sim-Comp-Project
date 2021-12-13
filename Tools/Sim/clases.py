c4ab314fbff78c08ff6f3828a44402600c503563
David Orlando <dquesada969x@gmail.com>
2021-12-13 13:11:07 -0500

    :.

--- Tools/Sim/clases.py
+++ Tools/Sim/clases.py
@@ -2,7 +2,7 @@ from typing import List
 
 from enum import Enum
 
-
+import random
 class RocketStatus(Enum):
    
     GOOD = 0
@@ -19,6 +19,8 @@ class Rocket:
         self.satellites = satellites
         self.status = RocketStatus.GOOD
         self.life = 100
+        self.windResistence = 20
+        self.speed = 0
 
     
 
@@ -29,7 +31,25 @@ class Satellite:
 
 class Junk:
 
-    def __init__(self, size: float):
+    def __init__(self, size: float, position):
         self.size = size
+        self.position =position
 
+    def __str__(self) -> str:
+        return f"size:{self.size}-position:{self.position}"
+
+def generateJunk():
+    junks = []
+    for _ in range(1, random.randint(5, 100)):
+        x,y,z = random.randint(1,100),random.randint(1,100),random.randint(1,100)
+        junks.append(Junk(random.randint(1,10),(x,y,z)))
+    return junks
+
+for junk in generateJunk():
+    print(junk)
     
+class EnvironmentMap:
+
+    def __init__(self, maxX, maxY, maxZ):
+        ...
+
