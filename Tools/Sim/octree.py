f64bf9c1ba32805f7861830b2dd322ce33e16d46
Daniel <danieldelacruzprieto@gmail.com>
2021-12-27 00:25:01 -0500

    adding some changes to octree class

--- Tools/Sim/octree.py
+++ Tools/Sim/octree.py
@@ -1,9 +1,9 @@
 
 from typing import List
-
+from Vector3 import *
 
 class Node:
-  def __init__(self, region, objects):
+  def __init__(self, region : Region, objects):
     self.objects = objects
     self.region = region
     self.childs = []
@@ -16,11 +16,43 @@ class Objects:
     self.radius = radius
 
 
+def giveMeOctant (center : Vector3 , position: Vector3):
+  newpos = position - center
+  newpos = newpos.sig()
+  if newpos.x == 0 or newpos.y == 0 or newpos.z == 0:
+    return None
+  xx = newpos.x if newpos.x > 0 else 0
+  yy = newpos.y if newpos.y > 0 else 0
+  zz = newpos.z if newpos.z > 0 else 0
+  return xx * 4 | yy * 2 | zz
+  
 
+class Region:
+  def __init__(self, center: Vector3 , radio : int) -> None:
+    self.center = center
+    self.radio = radio
+
+def clasifyObjects (objects: List [Objects] , center: Vector3):
+  #building list for clasify objects
+  clasifyList = [[] for i in range (9)] 
+  for obj in objects:
+    octant = giveMeOctant(center=center,position=obj.position)
+    if octant is None:
+      clasifyList[8].append(obj)
+    else: 
+      clasifyList[octant].append(obj)
+  return clasifyList
 
 class Octree:
   def __init__(self, objects):
     self.root = Node(objects)
 
   def BuildTree(self, node: Node, objects: List[Objects]):
-    for obj in objects:
+    obj = clasifyObjects(objects= objects, center=node.region)
+
+
+a = Vector3(-2,-3,4)
+b = Vector3(5,6,7)
+
+center = Vector3(0,0,0)
+print (giveMeOctant(center, position=a))
