47d829f1af756ff042988b1f095d6d0eed1fad81
Daniel <danieldelacruzprieto@gmail.com>
2022-01-16 17:23:00 -0500

    comentado el codigo del octree para que sea mas legible el codigo

--- Tools/Sim/octree.py
+++ Tools/Sim/octree.py
@@ -6,21 +6,6 @@ from objects import *
 MINRADIUS =1
 
 
-
-# class obj:
-#   def __init__(self, position, mass=1, radius=1,name=None):
-#     self.mass = mass
-#     self.position = position
-#     self.radius = radius
-#     self.name = name
-
-#   def move (self , newPosition)-> None:
-#     self.position = newPosition
-
-
-#   def __str__(self) -> str:
-#     return 'name: ' + str(self.name) + ' mass: ' + str(self.mass) + ' position: ' + str(self.position) + '  radius: ' + str(self.radius)
-
 class Node:
   def __init__(self, region: Region, objects: List[obj] ,parent , depth:int=0, isLeafNode:bool = True ):
     self.objects = objects
@@ -30,29 +15,29 @@ class Node:
     self.depth = depth
     self.isLeafNode = True 
 
-def giveMeOctantOfRegion (region: Region, object:obj):
-  if (region.center.x < object.position.x):
-    if (region.center.y < object.position.y):
-      if (region.center.z < object.position.z):
-        return 0 
-      else: 
-        return 1
-    else:
-      if (region.center.z < object.position.z):
-        return 2
-      else:
-        return 3 
-  else:
-    if (region.center.y < object.position.y): 
-      if (region.center.z < object.position.z):
-        return 4 
-      else:
-        return 5 
-    else:
-      if (region.center.z < object.position.z):
-        return 6 
-      else:
-        return 7
+# def giveMeOctantOfRegion (region: Region, object:obj):
+#   if (region.center.x < object.position.x):
+#     if (region.center.y < object.position.y):
+#       if (region.center.z < object.position.z):
+#         return 0 
+#       else: 
+#         return 1
+#     else:
+#       if (region.center.z < object.position.z):
+#         return 2
+#       else:
+#         return 3 
+#   else:
+#     if (region.center.y < object.position.y): 
+#       if (region.center.z < object.position.z):
+#         return 4 
+#       else:
+#         return 5 
+#     else:
+#       if (region.center.z < object.position.z):
+#         return 6 
+#       else:
+#         return 7
 
 def giveMeOctant (center : Vector3 , position: Vector3):
   newpos = position - center
@@ -68,7 +53,7 @@ def checkRange (c:int , r:int ,p:int):
   return (p <= (c + r)) and (p >= (c - r)) 
 
 
-# dado una region y un punto determia si el punto esta dentro del objeto 
+# dado una region y un punto determina si el punto esta dentro del objeto 
 def ObjectInside(region: Region , vector: Vector3):
   inside = True 
   inside = inside and checkRange(region.center.x,region.radio,vector.x)
@@ -78,8 +63,8 @@ def ObjectInside(region: Region , vector: Vector3):
 
 # Dada una lista de objetos y un punto como centro de referencia 
 # de alguna region retorna una lista de listas de objetos de 
-# cardinalidad 9, el ultimo posicion del array es para los 
-# objetos que estan sobre algun plano princial de la region en cuestion
+# cardinalidad 9, el ultimo posición del array es para los 
+# objetos que están sobre algún plano principal de la region en cuestión
 def clasifyObjects (objects: List [obj] , center: Vector3):
   #building list for clasify objects
   clasifyList = dict() 
@@ -199,23 +184,30 @@ except ImportError:
     np = None
 
 
-class OctNode(object):
+class Node(object):
     def __init__(self, position, size, depth, data):
+
+        # posición del nodo 
         self.position = position
+
+        # tamaño del nodo 
         self.size = size
+
+        # profundidad del nodo
         self.depth = depth
 
+        # true si el nodo es una hoja 
         self.isLeafNode = True
 
-        ## store our object, typically this will be one, but maybe more
+        ## para guardar los objetos , normalmente es uno, pero puede ser mas
         self.data = data
 
-        ## might as well give it some emtpy branches while we are here.
+        ## las hojas del nodo se ponen a None (inicialmente no se tiene ningún hijo) 
         self.branches = [None, None, None, None, None, None, None, None]
 
         half = size / 2
 
-        ## The cube's bounding coordinates
+        ## Las coordenadad de los extremos del nodo
         self.lower = (position[0] - half, position[1] - half, position[2] - half)
         self.upper = (position[0] + half, position[1] + half, position[2] + half)
 
@@ -228,16 +220,29 @@ class OctNode(object):
 
 class Octree(object):
     def __init__(self, worldSize, origin=(0, 0, 0), max_type="nodes", max_value=10):
-        self.root = OctNode(origin, worldSize, 0, [])
+        '''
+        inicializando el octree 
+        '''
+
+        # este es la raíz del árbol octree 
+        self.root = Node(origin, worldSize, 0, [])
+
+        # este es el tamaño del espacio que va a representar el octree 
         self.worldSize = worldSize
+
+        # el limite de los nodos 
+
         self.limit_nodes = (max_type=="nodes")
+        
+        # el limite del octree 
         self.limit = max_value
 
     @staticmethod
     def CreateNode(position, size, objects):
-        """This creates the actual OctNode itself."""
-        return OctNode(position, size, objects)
+        """Este crea al nodo actual el mismo"""
+        return Node(position, size, objects)
 
+    # esto es para insertar un objeto nuevo en el octree 
     def insertNode(self, position, objData=None):
         if np:
             if np.any(position < self.root.lower):
@@ -256,16 +261,24 @@ class Octree(object):
         return self.__insertNode(self.root, self.root.size, self.root, position, objData)
 
     def __insertNode(self, root, size, parent, position, objData):
-        """Private version of insertNode() that is called recursively"""
+        """
+        método privado para insertar elementos en el octree
+        """
         if root is None:
+
+            # en la variable pos ponemos la posición del padre
             pos = parent.position
 
+            # calculamos el offset (que es como el "radio" del cubo)
             offset = size / 2
 
+            # obtenemos la rama (octante) donde se insertara el nuevo objeto  
             branch = self.__findBranch(parent, position)
 
+            # inicializamos un nuevo centro 
             newCenter = (0, 0, 0)
 
+            # tratamos de calcular el nuevo centro del octante donde se ubicara el objeto 
             if branch == 0:
                 newCenter = (pos[0] - offset, pos[1] - offset, pos[2] - offset )
             elif branch == 1:
@@ -283,9 +296,10 @@ class Octree(object):
             elif branch == 7:
                 newCenter = (pos[0] + offset, pos[1] + offset, pos[2] + offset )
 
-            return OctNode(newCenter, size, parent.depth + 1, [objData])
+            # se retorna un nuevo nodo con las nuevas dimensiones 
+            return Node(newCenter, size, parent.depth + 1, [objData])
 
-        #else: are we not at our position, but not at a leaf node either
+        #else: no estamos en nuestra posición, pero tampoco en una hoja
         elif (
             not root.isLeafNode
             and
@@ -295,38 +309,65 @@ class Octree(object):
                 (root.position != position)
             )
         ):
+            # buscar la rama en la que s insertaria 
             branch = self.__findBranch(root, position)
+
+            # se calcula el nuevo size 
             newSize = root.size / 2
+
+            # se crea el nuevo octree con los objetos dentro de esta nueva region
+            # y se asigna como hijo en el branch que le corresponde 
             root.branches[branch] = self.__insertNode(root.branches[branch], newSize, root, position, objData)
 
-        # else, is this node a leaf node with objects already in it?
+
+        # else , es este un nodo hoja con objetos todavía en el
         elif root.isLeafNode:
             if (
                 (self.limit_nodes and len(root.data) < self.limit)
                 or
                 (not self.limit_nodes and root.depth >= self.limit)
             ):
+                # se añade el nuevo objeto a la data del node 
                 root.data.append(objData)
             else:
+                
+                # se añade el objeto a la data del nodo
                 root.data.append(objData)
+
+                #se crea una nueva variable y se la asigna la lista de data del nodo  
                 objList = root.data
+
+                # Se desreferencia la lista d e data del nodo 
                 root.data = None
+
+                # el nodo deja de ser una hoja 
                 root.isLeafNode = False
+
+                # se calcula el nuevo size del nodo 
                 newSize = root.size / 2
 
+                # se recorre la lista de objetos y se crean los nodos 
+                # con una profundidad mayor 
                 for ob in objList:
                     if hasattr(ob, "position"):
                         pos = ob.position
                     else:
                         pos = ob
+                    
+                    # se busca el octante donde se insertara el nuevo objeto
                     branch = self.__findBranch(root, pos)
+
+                    # se crean los nuevos branch con los sub_octree que le corresponden
                     root.branches[branch] = self.__insertNode(root.branches[branch], newSize, root, pos, ob)
+        
+        # se retorna el root modificado 
         return root
 
+    # retorna los objetos que están en una posición dada 
     def findPosition(self, position):
         """
-        Basic lookup that finds the leaf node containing the specified position
-        Returns the child objects of the leaf, or None if the leaf is empty or none
+        Búsqueda básica que encuentra el nodo hoja que contiene la posición especificada
+        Devuelve los objetos secundarios de la hoja, o Ninguno si la hoja está vacía o ninguno 
         """
         if np:
             if np.any(position < self.root.lower):
@@ -342,14 +383,22 @@ class Octree(object):
 
     @staticmethod
     def __findPosition(node, position, count=0, branch=0):
+        # si el nodo es una hoja se retorna la data que contiene 
         if node.isLeafNode:
             return node.data
+        
+        # se calcula el octante a buscar  
         branch = Octree.__findBranch(node, position)
+        
+        # se obtiene el sub_octree en el cual están los posibles nodos a buscar 
         child = node.branches[branch]
         if child is None:
             return None
+        
+        # se manda a buscar en el nodo hijo donde potencialmete se encuentren los elementos
         return Octree.__findPosition(child, position, count + 1, branch)
 
+    # encuentra el octante dado una rama determinada
     @staticmethod
     def __findBranch(root, position):
         index = 0
@@ -361,15 +410,17 @@ class Octree(object):
             index |= 1
         return index
 
+    # iteracion en profundidad primero 
     def iterateDepthFirst(self):
-        """Iterate through the octree depth-first"""
+        """iterar en el octree en profundidad primero"""
         gen = self.__iterateDepthFirst(self.root)
         for n in gen:
             yield n
 
+    # metodo privado patra iterar primero en profundidad
     @staticmethod
     def __iterateDepthFirst(root):
-        """Private (static) version of iterateDepthFirst"""
+        """private metodo para la busqueda en el octree con busqueda primero en profundidad"""
 
         for branch in root.branches:
             if branch is None:
