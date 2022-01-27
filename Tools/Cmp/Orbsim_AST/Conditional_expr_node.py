784446da2fdffbdf169fdb03b3e4d16a963db51c
dmguezjaviersnet <dmguezjaviersnet@gmail.com>
2022-01-27 17:32:57 -0500

    .:

--- Tools/Cmp/Orbsim_AST/Conditional_expr_node.py
+++ Tools/Cmp/Orbsim_AST/Conditional_expr_node.py
@@ -1,11 +1,6 @@
 from ExpressionNode import ExpressionNode
 from dataclasses import dataclass
 
-<<<<<<< HEAD:Tools/Cmp/AST_SL/ConditionalExprNode.py
-=======
-
-
->>>>>>> 1b4cb44d312d0599c758dbf5b303aac308c96332:Tools/Cmp/Orbsim_AST/Conditional_expr_node.py
 @dataclass
 class ConditionalExprNode(ExpressionNode):
     if_expr: ExpressionNode
