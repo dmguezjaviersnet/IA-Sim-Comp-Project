02a468704f377437101a17c9f343a92ff8dfd97d
David Orlando <dquesada969x@gmail.com>
2022-02-10 09:00:11 -0500

    agregado para serializar con pickle

--- orb_simulator/tools.py
+++ orb_simulator/tools.py
@@ -1,1 +1,12 @@
 EPSILON = ''
+import pickle
+
+def make_pickle_file(filename, data):
+    with open(f"{filename}.pickle", "wb") as outfile:
+        pickle.dump(data, outfile)
+
+def unpick_pickle_file(filename):
+    with open(filename, 'rb') as f:
+        data = pickle.load(f)
+    
+    return data
