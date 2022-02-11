938e4198beff93aea5fbb3c7d36a7d380b40b22d
David Orlando <dquesada969x@gmail.com>
2022-02-11 16:13:59 -0500

    :.

--- orb_simulator/orbsim_language/built_in_funcs.py
+++ orb_simulator/orbsim_language/built_in_funcs.py
@@ -1,5 +1,6 @@
 from orbsim_language.orbsim_ast.string_node import StringNode
 from orbsim_language.instance import Instance
+from orbsim_language.orbsim_type import*
 
-def concat(s1: StringNode, s2: StringNode):
-    return Instance( s1 + s2)
+def concat(s1: 'Instance', s2: 'Instance'):
+    return Instance(IntType(), s1.value + s2.value)
