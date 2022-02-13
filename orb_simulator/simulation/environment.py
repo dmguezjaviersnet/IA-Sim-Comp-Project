bb394fb4e33e724c45bb5088252da1baeebad8f8
Daniel de la Cruz <danieldelacruzprieto@gmail.com>
2022-02-12 23:10:43 -0500

    annadiendo metodos para obtener todo los launchpads del environment

--- orb_simulator/simulation/environment.py
+++ orb_simulator/simulation/environment.py
@@ -1,3 +1,5 @@
+from orbsim_simulation_entities import OrbsimObj , Factory , Vector3 , Launchpad , Junk, Vector2
+from typing import *
 class cell: 
   def __init__(self, x , y):
     self._x = x
@@ -25,6 +27,20 @@ class Environment:
       for col in row:
         print(col, end ='')
       print()
+
+  def _in_range (self , x: int , y: int ):
+    return ((x>=0) and (x < self._height) and (y >= 0 ) and (y < self._width))
+
+  def getting_all_launchpad(self, pos: Vector2 , radius: int ):
+    launchpad = []
+    for i in range (pos.x - radius, pos.x + radius):
+      for j in range (pos.y - radius, pos.y + radius):
+        if self._in_range(i,j):
+          for item in self._cells[i][j]._elements:
+            if type(item) is Launchpad:
+              launchpad.append(item)
+
+
 
 env = Environment(1000,1000)
 # env.print()
