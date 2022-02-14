5cddc796e3079f48062026f4febefb5a0dbbbd86
Daniel de la Cruz <danieldelacruzprieto@gmail.com>
2022-02-14 02:18:05 -0500

    combiando el nombre de loc a position

--- orb_simulator/simulation/orbsim_simulation_entities/person.py
+++ orb_simulator/simulation/orbsim_simulation_entities/person.py
@@ -12,8 +12,8 @@ from config_variables import T_WAITING_TO_INTERACT
 # Esta clase representa a una persona que es un agente
 # que va a interactuar con los demas agentes de la simulacion 
 class Person(Agent):
-  def __init__ (self, loc: Vector3 , unique_id: uuid.UUID):
-    super().__init__(loc, unique_id)
+  def __init__ (self, position: Vector3 , unique_id: uuid.UUID):
+    super().__init__(position, unique_id)
 
   # Para interactuar con las demas agentes y generar nuevos objetos simulacion
   # digamos satelites para poder annadir mas elementos a la simulacion 
