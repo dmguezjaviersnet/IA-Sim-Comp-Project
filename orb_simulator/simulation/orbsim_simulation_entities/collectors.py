d644d3be64bb32a7a0eac7c5da66dcedf6145396
Daniel de la Cruz <danieldelacruzprieto@gmail.com>
2022-02-14 03:41:59 -0500

    actualizando cosas en los collectors

--- orb_simulator/simulation/orbsim_simulation_entities/collectors.py
+++ orb_simulator/simulation/orbsim_simulation_entities/collectors.py
@@ -1,5 +1,5 @@
 from orbsim_simulation_entities.agent import Agent
-from orbsim_simulation_entities.elements_3d import Vector3
+from orbsim_simulation_entities.elements_3d import Vector3, Vector2
 from enum import Enum
 
 class STATUSCOLLECTOR(Enum):
@@ -9,28 +9,42 @@ class STATUSCOLLECTOR(Enum):
 
 
 class Collectors(Agent):
-  def __init__(self, loc: Vector3, unique_id, params) -> None:
-    super().__init__(loc, unique_id, params)
+  def __init__(self, position: Vector3, unique_id, params) -> None:
+    super().__init__(position, unique_id, params)
     self.status = STATUSCOLLECTOR.HALFFULL
+    self._way_to_go = []
   
 
-  def _getdetermination():
+  def _get_determination():
     det = [(STATUSCOLLECTOR.FULL , 'empty'),
           (STATUSCOLLECTOR.EXHAUSTED, 'descend'),
           (STATUSCOLLECTOR.HALFFULL,'collect')]
     return det
 
-  def _rejuvenate(self):
-    pass
+  def _rejuvenate(self, env):
+    pass 
 
-  def _emptyCollector():
-    pass
+
+  def move (self):
+    if len(self._way_to_go) > 0 :      
+      next_move = self._way_to_go.pop(0)
+      self.position = Vector3(next_move.x, next_move.y)
+    else: 
+      self.position = Vector3.random(100)
+
+
+  def _emptyCollector(self, env):
+    position2d = Vector2(self.position.x, self.position.y)
+    launcpad_more_close = env.get_the_most_close_launcpad(position2d)
+    walk = env.get_the_best_way (position2d, Vector2(launcpad_more_close.position.x, launcpad_more_close.position.y))
+    self._way_to_go = walk
+
 
   def _collect(self):
     pass
 
   def collectgarbage(self):
-    determ = self._getdetermination()
+    determ = self._get_determination()
     for status in determ:
       if status[0] == STATUSCOLLECTOR.FULL:
         self._emptyCollector()
