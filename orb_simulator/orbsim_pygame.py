0acc2e2f4a715c91073492ec75b94117ef5d1f2c
dmguezjaviersnet <dmguezjaviersnet@gmail.com>
2022-03-01 12:14:34 -0500

    buscando error en el cómputo de los nodos vecinos del quadtree

--- orb_simulator/orbsim_pygame.py
+++ orb_simulator/orbsim_pygame.py
@@ -1,4 +1,5 @@
 from asyncio import subprocess
+from operator import le
 from typing import List
 import pygame
 from sprites_and_graph_ent import ElipticOrbit, SpaceDebris, Launchpad, Satellite, SpaceDebrisCollector
@@ -200,22 +201,44 @@ class PygameHandler():
                     object.is_colliding = False
                     qTree.insert(object)
                 
-                for agent in self.agents:
-                    qTree.insert(agent)
+                qTree.insert(self.earth)
+                
+                # for agent in self.agents:
+                #     qTree.insert(agent)
                 
                 global leaves
                 for leaf in leaves:
                     leaf.check_collisions()
                     leaf.find_neighbors()
+                    # for neigh in leaf.neighbors:
+                    #     print(f'leaf centered at {leaf.center_x} x and {leaf.center_y} y --------> neighbor centered at {neigh.center_x} x {neigh.center_y} y')
+                    
+                    # print(len(leaf.neighbors))
 
-                for agent in self.agents:
-                    agent.options()
-                    agent.pursue_goal()
+                queue = [leaves[0]]
+                visited = []
+                while queue:
+                    elem = queue.pop(0)
+                    for neigh in elem.neighbors:
+                        if not neigh in visited:
+                            visited.append(neigh)
+                            queue.append(neigh)
+                            pygame.draw.rect(self.screen, (255, 0, 0), 
+                                            [neigh.bounding_box_tl[0], neigh.bounding_box_tl[1], 
+                                            neigh.bounding_box_br[0] - neigh.bounding_box_tl[0], neigh.bounding_box_br[1] - neigh.bounding_box_tl[1]])
+                
+                pygame.draw.circle(self.screen, (0, 255, 0), 
+                                [leaves[50].bounding_box_tl[0], leaves[50].bounding_box_tl[1]], 20)
 
-               
+                print(f'Initial leave is at {leaves[50].bounding_box_br[0]} x and {leaves[50].bounding_box_br[1]} y')
 
-                qTree.insert(self.earth)
+                # print(f'THERE ARE {len(leaves)} leaves')
 
+                # for agent in self.agents:
+                #     agent.options()
+                #     agent.pursue_goal()
+
+                
                 
                 # pygame.draw.rect(self.screen, BLUE, self.main_region_rect, 1)
                 # end = time.time()
@@ -229,7 +252,7 @@ class PygameHandler():
                 self.space_debris_group.draw(self.screen)
                 self.satellite_group.draw(self.screen)
                 self.earth_group.draw(self.screen)
-                self.space_debris_collector_group.draw(self.screen)
+                # self.space_debris_collector_group.draw(self.screen)
 
                 for obj in self.objects:
                     obj.draw_collision(self.screen)
@@ -242,7 +265,7 @@ class PygameHandler():
                 # self.space_debris_group.update()
                 self.earth_group.update()
                 self.satellite_group.update()
-                self.space_debris_collector_group.update()
+                # self.space_debris_collector_group.update()
                 counter_time += 0.01
                 self.clock.tick(60)
                 
